e760e654d1ee20a0e062732f1e09dcb267f6c32b
Aubrey Carter <aubrcart224@ed.amdsb.ca>
2022-04-22 10:03:46 -0400

    weather app

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -14,7 +14,7 @@ namespace XMLWeather
     public partial class Form1 : Form
     {
         // TODO: create list to hold day objects
-
+        public static List<Day> days = new List<Day>();
 
         public Form1()
         {
@@ -34,11 +34,37 @@ namespace XMLWeather
 
             while (reader.Read())
             {
-                //TODO: create a day object
+                //create a day object
+                Day newDay = new Day();
 
-                //TODO: fill day object with required data
+                //fill day object with required data
+                reader.ReadToFollowing("time");
+                newDay.date = reader.GetAttribute("day");
 
-                //TODO: if day object not null add to the days list
+                reader.ReadToFollowing("temperature");
+                newDay.tempLow = reader.GetAttribute("min");
+                newDay.tempHigh = reader.GetAttribute("max");
+
+                reader.ReadToFollowing("wind");
+                newDay.windSpeed = reader.GetAttribute("speed");
+
+                reader.ReadToFollowing("precipitation");
+                newDay.precipitation = reader.GetAttribute("probability");
+                newDay.precipitation = reader.GetAttribute("type");
+                
+
+                reader.ReadToFollowing("clouds");
+                newDay.visibility = reader.GetAttribute("value");
+
+                
+
+
+                //if day object not null add to the days list
+                if (newDay.date != null)
+                {
+                    days.Add(newDay);
+                }
+
             }
         }
 
@@ -48,7 +74,22 @@ namespace XMLWeather
             XmlReader reader = XmlReader.Create("http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0");
 
             //TODO: find the city and current temperature and add to appropriate item in days list
+            reader.ReadToFollowing("city");
+            days[0].location = reader.GetAttribute("name");
 
+            reader.ReadToFollowing("temperature");
+            days[0].currentTemp = reader.GetAttribute("value");
+
+            reader.ReadToFollowing("wind");
+            days[0].windSpeed = reader.GetAttribute("speed");
+
+            reader.ReadToFollowing("precipitation");
+            days[0].precipitation = reader.GetAttribute("probability");
+            days[0].precipitation = reader.GetAttribute("type");
+
+
+            reader.ReadToFollowing("clouds");
+            days[0].visibility = reader.GetAttribute("value");
         }
 
 
